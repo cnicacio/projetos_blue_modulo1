150537bee185bf7f314cb29387169b222e93311e
Carlos Nicacio <carlosnicacio94@gmail.com>
2021-06-21 19:09:40 -0300

    Entrega

--- projeto_02_jokenpo.py
+++ projeto_02_jokenpo.py
@@ -37,20 +37,16 @@ tesoura = '''
 ---.__(___)
 '''
 
-jogador = computador = 0
+jogador = computador = empate = 0
 
 while True:
     jogadas = int(input('Digite o número de jogadas: '))
     for c in range(jogadas): # para o número de jogadas definidas, repete-se o que segue
-        jogada = str(input(''' FAÇA A SUA JOGADA
-        Pedra [1] 
-        Papel [2] 
-        Tesoura [3]      
-        '''))
+        jogada = int(input('FAÇA A SUA JOGADA\nPedra [1]\nPapel [2]\nTesoura [3]\n'))
 
-        while jogada not in '123': # tratamento de erro para o jogador inserir a jogada corretamente
-            jogada = str(input('Digite corretamente. Pedra [1], papel [2] ou tesoura? [3] '))
-        comp_jogada = str(randint(1,3)) # computador "sorteará" um número entre 1 e 3 para fazer a sua jogada aleatória
+        while jogada < 1 or jogada > 3: # tratamento de erro para o jogador inserir a jogada corretamente
+            jogada = int(input('Digite corretamente. Pedra [1], papel [2] ou tesoura? [3] '))
+        comp_jogada = randint(1,3) # computador "sorteará" um número entre 1 e 3 para fazer a sua jogada aleatória
 
         print('JO')
         sleep(1)
@@ -58,60 +54,45 @@ while True:
         sleep(1)
         print('PO')
 
-        if comp_jogada == '1': # COMPUTADOR JOGOU PEDRA
+        if comp_jogada == 1: # COMPUTADOR JOGOU PEDRA
             print(f'O computador jogou:\n{pedra}')
-            if jogada == '1':
-                print(f'Você jogou:\n{pedra}\n\nEMPATE!')
-            if jogada == '2':
-                print(f'Você jogou:\n{papel}\n\nVOCÊ VENCEU!')
-                jogador += 1
-            if jogada == '3':
-                print(f'Você jogou:\n{tesoura}\n\nVOCÊ PERDEU!')
-                computador += 1
+        if comp_jogada == 2: # COMPUTADOR JOGOU PAPEL
+                print(f'O computador jogou:\n{papel}')
+        if comp_jogada == 3: # COMPUTADOR JOGOU TESOURA
+                print(f'O computador jogou:\n{tesoura}')
 
-        if comp_jogada == '2': # COMPUTADOR JOGOU PAPEL
-            print(f'O computador jogou:\n{papel}')
-            if jogada == '1':
-                print(f'Você jogou:\n{pedra}\n\nVOCÊ PERDEU!')
-                computador += 1
-            if jogada == '2':
-                print(f'Você jogou:\n{papel}\n\nEMPATE!')
-            if jogada == '3':
-                print(f'Você jogou:\n{tesoura}\n\nVOCÊ VENCEU!')
-                jogador += 1
+        if jogada == 1: # USUÁRIO JOGOU PEDRA
+            print(f'Você jogou:\n{pedra}')
+        elif jogada == 2: # USUÁRIO JOGOU PAPEL
+            print(f'Você jogou:\n{papel}')
+        elif jogada == 3: # USUÁRIO JOGOU TESOURA
+            print(f'Você jogou:\n{tesoura}')
 
-        if comp_jogada == '3': # COMPUTADOR JOGOU TESOURA
-            print(f'O computador jogou:\n{tesoura}')
-            if jogada == '1':
-                print(f'Você jogou:\n{pedra}\n\nVOCÊ VENCEU!')
-                jogador += 1
-            if jogada == '2':
-                print(f'Você jogou:\n{papel}\n\nVOCÊ PERDEU!')
-                computador += 1
-            if jogada == '3':
-                print(f'Você jogou:\n{tesoura}\n\nEMPATE!')
-    
-    novamente = str(input('Deseja jogar novamente [S/N]? ')).strip().upper()[0] # ao fim, pergunta-se se o jogador quer jogar novamente
+        if (jogada - comp_jogada) == 0: # EMPATE (NÚMEROS IGUAIS NA RODADA)
+            print('RODADA EMPATADA!')
+            empate += 1
+        elif (jogada - comp_jogada) == 1 or (jogada - comp_jogada) == -2: # COMBINAÇÃO DE RESULTADOS PARA A VITÓRIA DO USUÁRIO
+            print('VOCÊ VENCEU!')
+            jogador += 1
+        elif (jogada - comp_jogada) == -1 or (jogada - comp_jogada) == 2: # COMBINAÇÃO DE RESULTADOS PARA A VITÓRIA DO COMPUTADOR
+            print('VOCÊ PERDEU!')
+            computador += 1
+
+    novamente = str(input('Deseja jogar novamente [S/N]? ')).strip().upper()[0] # ao fim do range(jogadas), ou seja, após o número de rodadas definida pelo usuário, pergunta-se se o jogador quer jogar novamente
     while novamente not in 'SN': # tratamento de erro para o jogador inserir a opção corretamente
         novamente = str(input('Resposta inválida! Deseja jogar novamente [S/N]? ')).strip().upper()[0]
-    if novamente == 'N':
+    if novamente == 'N': # se a resposta for não, sai do loop e vai para as verificações de vencedor
         break
 
+print(f'''Você venceu {jogador} rodadas 
+O computador venceu {computador} rodadas
+Houveram {empate} rodadas empatadas''') # contagem dos vencedores das rodadas
+
 if jogador > computador: # o vencedor é o jogador
-    print(f'''Você venceu {jogador} rodadas
-O computador venceu {computador} rodadas
+    print(f'Você é o grande vencedor!')
 
-Você é o grande vencedor!
-    ''')
 elif jogador < computador: # o vencedor é o computador
-    print(f'''Você venceu {jogador} rodadas
-O computador venceu {computador} rodadas
+    print(f'O computador é o grande vencedor!')
 
-O computador é o grande vencedor!
-    ''')
 else: # em caso de empate
-    print(f'''Você venceu {jogador} rodadas
-O computador venceu {computador} rodadas
-
-O grande duelo foi empatado!
-    ''')
+    print(f'O grande duelo foi empatado!')
